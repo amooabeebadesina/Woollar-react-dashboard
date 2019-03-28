c44d6a71e2f2a8d0a9fc4829138b82678ba92b00
amooabeebadesina <amooabeeb0@gmail.com>
2019-03-28 19:22:05 +0100

    Change router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import './App.scss';
-import { Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Routes from './routes';
 import store from './store';
-import { history } from './utils/helpers';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router history={history}>
+        <Router>
           <div className="App">
             <Routes />
           </div>
